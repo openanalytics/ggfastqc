f5c4e631f8c8b9849f7d21e02553cbe99fdd51c4
Arunkumar Srinivasan <arunkumar.srinivasan@openanalytics.eu>
2015-09-01 21:41:10 +0200

    Initial commit with DESCRIPTION and package info.

---
+++ R/ggfastqc.R
@@ -0,0 +1,10 @@
+#' ggfastqc: ggplots of FastQC summary data
+#' 
+#' @aliases ggfastqc ggfastqc-package
+#' @docType package
+#' @name ggfastqc
+#' @import methods
+#' @import data.table
+#' @import ggplot2
+#' @seealso \code{\link{get_counts}}
+NULL
