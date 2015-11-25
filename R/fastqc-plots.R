06b5cced8171167cbd9206282ce0ced680382735
Arunkumar Srinivasan <arunkumar.srinivasan@openanalytics.eu>
2015-11-25 15:10:01 +0100

    Better structure for easily facetting plots.

--- R/fastqc-plots.R
+++ R/fastqc-plots.R
@@ -1,32 +1,29 @@
 #' @title Plot FastQC GC\% stats
 #' @export
-plot_gc_stats <- function(..., type=c("ggplot2", "rbokeh"), group=NULL) {
+plot_gc_stats <- function(..., type=c("ggplot2", "rbokeh")) {
 
   ll = list(...)
   gc = lapply(ll, function(l) rbindlist(lapply(l, `[[`, "Basic Statistics")))
   if (is.null(names(ll)))
     setattr(gc, 'names', names(ll))
   gc = rbindlist(gc, idcol=TRUE)[`#Measure` == "%GC"]
-  gc[, c("sample_name", "pairs") := split_sample(sample)]
-  setnames(gc, c("sample", "sample_name", "Value"), c("Sample (Pairs)", "Sample", "Percent"))
-  gc[, Percent := as.numeric(as.character(Percent))]
-  gc[, Sample  := as.character(Sample)]
-  if (is.data.table(group)) {
-    if (!all(c("Sample", "Condition") %in% names(group)))
-      stop("Columns 'Sample' and 'Condition' should be present in data.table 'group'.")
-    group = copy(group)[, Sample := as.character(Sample)][]
-    gc[group, Condition := i.Condition, on="Sample"]
-  } else gc[, Condition := ""]
-
+  setnames(gc, "Value", "percent")[, percent := as.numeric(as.character(percent))]
   type = match.arg(type)
   switch(type, 
     ggplot2 = {
       if (!require(ggplot2))
         stop("Package 'ggplot2' is not available.")
-      pl = ggplot(gc, aes(x=`Sample (Pairs)`, y=Percent))
-      pl = pl + geom_bar(stat="identity", aes(fill=Condition, group=`Sample (Pairs)`))
-      pl = pl + ylim(0, 100)
-      pl = pl + facet_wrap(~ .id, ncol=1L, scales="free_x")
+      pl = ggplot(gc, aes(x=sample_name, y=percent)) + 
+            geom_bar(stat="identity", aes(fill=group, group=sample_name)) + 
+            ylim(0, 100) + 
+            facet_wrap(~ .id, ncol=1L, scales="free_x") + 
+            theme_bw() + 
+            theme(axis.text.x = element_text(angle = 45L, hjust = 1, size=14), 
+                axis.text.y = element_text(size = 14),  
+                text = element_text(size=18), 
+                panel.grid.major = element_blank()) + 
+             xlab("Sample") + ylab("GC %") + 
+             scale_fill_brewer(name = "Group", palette="Set1")
     }, 
     rbokeh = {
       stop("Not yet implemented.")
@@ -36,37 +33,29 @@ plot_gc_stats <- function(..., type=c("ggplot2", "rbokeh"), group=NULL) {
 
 #' @title Plot FastQC total sequences stats
 #' @export
-plot_total_sequence_stats <- function(..., type=c("ggplot2", "rbokeh"), group=NULL) {
+plot_total_sequence_stats <- function(..., type=c("ggplot2", "rbokeh")) {
 
   ll = list(...)
   ts = lapply(ll, function(l) rbindlist(lapply(l, `[[`, "Basic Statistics")))
   if (is.null(names(ll)))
     setattr(ts, 'names', names(ll))
   ts = rbindlist(ts, idcol=TRUE)[`#Measure` == "Total Sequences"]
-  ts[, c("sample_name", "pairs") := split_sample(sample)]
-  ts[, Value := as.numeric(as.character(Value))/1e6L]
-  setnames(ts, c("sample", "sample_name", "Value"), c("Sample (Pairs)", "Sample", "Counts (*1e6)"))
-  ts[, Sample := as.character(Sample)]
-  if (is.data.table(group)) {
-    if (!all(c("Sample", "Condition") %in% names(group)))
-      stop("Columns 'Sample' and 'Condition' should be present in data.table 'group'.")
-    group = copy(group)[, Sample := as.character(Sample)][]
-    ts[group, Condition := i.Condition, on="Sample"]
-  } else ts[, Condition := ""]
-
+  setnames(ts, "Value", "counts")[, counts  := as.numeric(as.character(counts))/1e6L]
   type = match.arg(type)
   switch(type, 
     ggplot2 = {
       if (!require(ggplot2))
         stop("Package 'ggplot2' is not available.")
-      pl = ggplot(ts, aes(x=`Sample (Pairs)`, y=`Counts (*1e6)`)) + 
-            geom_bar(stat="identity", aes(fill=Condition, group=`Sample (Pairs)`)) + 
+      pl = ggplot(ts, aes(x=sample_name, y=counts)) + 
+            geom_bar(stat="identity", aes(fill=group, group=sample_name)) + 
             facet_wrap(~ .id, ncol=1L, scales="free_x") + 
+            theme_bw() + 
             theme(axis.text.x = element_text(angle = 45L, hjust = 1, size=14), 
                 axis.text.y = element_text(size = 14),  
                 text = element_text(size=18), 
                 panel.grid.major = element_blank()) + 
-             xlab("Sample") + ylab("Counts (*1e6)") + scale_fill_brewer(palette="Set1")
+             xlab("Sample") + ylab("Counts (in million)") + 
+             scale_fill_brewer(name="Group", palette="Set1")
     }, 
     rbokeh = {
       stop("Not yet implemented.")
@@ -76,41 +65,38 @@ plot_total_sequence_stats <- function(..., type=c("ggplot2", "rbokeh"), group=NU
 
 #' @title Plot FastQC total sequences stats
 #' @export
-plot_dup_stats <- function(..., type=c("ggplot2", "rbokeh"), group=NULL) {
+plot_dup_stats <- function(..., type=c("ggplot2", "rbokeh")) {
 
   ll = list(...)
   dup = lapply(ll, function(l) {
           rbindlist(lapply(l, function(y) { 
             v = y[["Sequence Duplication Levels"]][1]
-            list(sample=v$sample, Duplication_Percent=as.numeric(names(v)[3L]))
+            list(group=v$group, sample_group = v$sample_group, 
+                 sample_name=v$sample_name, pairs = v$pairs, 
+                 Duplication_Percent=as.numeric(tail(names(v), 1L)))
           }))
         })
   if (is.null(names(ll)))
     setattr(dup, 'names', names(ll))
-  dup = rbindlist(dup, idcol=TRUE)[, c("sample_name", "pairs") := split_sample(sample)][]
-  setnames(dup, c("sample", "sample_name", "Duplication_Percent"), c("Sample (Pairs)", "Sample", "Dup %"))
-  dup[, Sample := as.character(Sample)]
-  if (is.data.table(group)) {
-    if (!all(c("Sample", "Condition") %in% names(group)))
-      stop("Columns 'Sample' and 'Condition' should be present in data.table 'group'.")
-    group = copy(group)[, Sample := as.character(Sample)][]
-    dup[group, Condition := i.Condition, on="Sample"]
-  } else dup[, Condition := ""]
+  dup = rbindlist(dup, idcol=TRUE)
+  setnames(dup, "Duplication_Percent", "dup_percent")
 
   type = match.arg(type)
   switch(type, 
     ggplot2 = {
       if (!require(ggplot2))
         stop("Package 'ggplot2' is not available.")
-      pl = ggplot(dup, aes(x=`Sample (Pairs)`, y=`Dup %`)) + 
-            geom_bar(stat="identity", aes(fill=Condition, group=`Sample (Pairs)`)) + 
+      pl = ggplot(dup, aes(x=sample_name, y=dup_percent)) + 
+            geom_bar(stat="identity", aes(fill=group, group=sample_name)) + 
             ylim(0, 100) + 
             facet_wrap(~ .id, ncol=1L, scales="free_x") + 
-             theme(axis.text.x = element_text(angle = 45L, hjust = 1, size=14), 
+            theme_bw() + 
+            theme(axis.text.x = element_text(angle = 45L, hjust = 1, size=14), 
                 axis.text.y = element_text(size = 14),  
                 text = element_text(size=18), 
                 panel.grid.major = element_blank()) + 
-             xlab("Sample") + ylab("Duplication %") + scale_fill_brewer(palette="Set1")
+             xlab("Sample") + ylab("Duplication %") + 
+             scale_fill_brewer(name="Group", palette="Set1")
     }, 
     rbokeh = {
       stop("Not yet implemented.")
@@ -120,42 +106,36 @@ plot_dup_stats <- function(..., type=c("ggplot2", "rbokeh"), group=NULL) {
 
 #' @title Plot FastQC per base sequence quality
 #' @export
-plot_sequence_quality <- function(..., type=c("ggplot2", "rbokeh"), group=NULL) {
+plot_sequence_quality <- function(..., type=c("ggplot2", "rbokeh")) {
 
   ll = list(...)
   seq = lapply(ll, function(l) rbindlist(lapply(l, `[[`, "Per base sequence quality")))
   if (is.null(names(ll)))
     setattr(seq, 'names', names(ll))
   seq = rbindlist(seq, idcol=TRUE)
-  seq[, names(seq)[-(1:3)] := lapply(.SD, function(x) as.numeric(as.character(x))), .SDcols=names(seq)[-(1:3)]]
-  seq[, c("sample_name", "pairs") := split_sample(sample)]
-  setnames(seq, c("sample", "sample_name", "pairs"), c("Sample (Pairs)", "Sample", "Pairs"))
-  seq[, Sample := as.character(Sample)
-    ][, Pairs := factor(Pairs)
+  colnames = setdiff(names(seq), c(".id", "group", "sample_group", "sample_name", "pairs", "#Base"))
+  seq[, (colnames) := lapply(.SD, function(x) as.numeric(as.character(x))), .SDcols=(colnames)]
+  seq[, pairs := factor(pairs)
     ][, `#Base` := factor(`#Base`, levels=unique(`#Base`))]
-  if (is.data.table(group)) {
-    if (!all(c("Sample", "Condition") %in% names(group)))
-      stop("Columns 'Sample' and 'Condition' should be present in data.table 'group'.")
-    group = copy(group)[, Sample := as.character(Sample)][]
-    seq[group, Condition := i.Condition, on="Sample"]
-  } else seq[, Condition := ""]
   type = match.arg(type)
   switch(type, 
     ggplot2 = {
       if (!require(ggplot2))
         stop("Package 'ggplot2' is not available.")
-      pl = ggplot(seq, aes(x=`#Base`, colour=Pairs, group=`Sample (Pairs)`)) + 
+      pl = ggplot(seq, aes(x=`#Base`, colour=pairs, group=sample_name)) + 
              geom_point(aes(y=Median)) + 
              geom_errorbar(aes(ymax=`90th Percentile`, ymin=`10th Percentile`)) + 
-             geom_line(aes(y=Mean, group=`Sample (Pairs)`)) + 
+             geom_line(aes(y=Mean, group=sample_name)) + 
              facet_wrap( ~ .id, scales="free_x", ncol=1L) + 
              geom_hline(aes(yintercept=30L), colour="gray25", linetype="dotted") + 
+             theme_bw() + 
              theme(axis.text.x = element_text(angle = 45L, hjust = 1, size=14), 
                 axis.text.y = element_text(size = 14),  
                 text = element_text(size=18), 
                 panel.grid.major = element_blank()) + 
-             xlab("Sample") + ylab("Read quality") + scale_fill_brewer(palette="Set1") + 
-             ylim(0L, max(seq$Mean, na.rm=TRUE)+1L)
+             xlab("#Base") + ylab("Read quality") + 
+             ylim(0L, max(seq$Mean, na.rm=TRUE)+1L) + 
+             scale_colour_brewer(name="Pairs", palette="Set1")
     }, 
     rboken = {
       stop("Not yet implemented.")
--- R/fastqc-tables.R
+++ R/fastqc-tables.R
@@ -1,20 +1,20 @@
 #' Extract FastQC summary tables
 #' @export
-fastqc <- function(dir) {
+fastqc <- function(dir, groups=NULL) {
 	# not exposing `id` argument here for now.
 	files = list.files(dir, pattern="^fastqc_data\\.txt$", full.names=TRUE, recursive=TRUE)
 	samples = gsub("_fastqc$", "", basename(dirname(files)))
-	all_samples = lapply(seq_along(samples), function(i) fastqc_tables(samples[i], files[i], id=NULL))
+	all_samples = lapply(seq_along(samples), function(i) fastqc_tables(samples[i], files[i], groups=groups))
 	setattr(all_samples, 'class', 'fastqc')
 	setattr(all_samples, 'names', samples)
 }
 
-fastqc_tables <- function(sample, fastqc_file, id=NULL) {
+fastqc_tables <- function(sample_name, fastqc_file, groups=NULL) {
 	doc = readLines(fastqc_file)
-	tables = extract_summary_tables(doc, sample, id)
+	tables = extract_summary_tables(doc, sample_name, groups)
 }
 
-extract_summary_tables <- function(doc, sample, id=NULL) {
+extract_summary_tables <- function(doc, sample_name, groups=NULL) {
 
 	idx = grep("^>>", doc)
 
@@ -32,24 +32,29 @@ extract_summary_tables <- function(doc, sample, id=NULL) {
 		bdy_row = strsplit(doc[(i+2L):(j-1L)], "\t", fixed=TRUE)
 		ans = setDT(transpose(bdy_row))
 		setnames(ans, hdr_row)
-		ans[, sample := sample]
-		if (!is.null(id)) {
-			ans[, id := id]
-			movecols = c("id", "sample")
-		} else movecols = "sample"
+		ans[, sample_name := sample_name
+		  ][, c("sample_group", "pairs") := split_sample(sample_name)]
+		if (is.data.table(groups)) {
+			groups = copy(groups)[, sample_group := as.character(sample)]
+			ans[groups, group := factor(i.group), on="sample_group"]
+		} else {
+			ans[, group := ""]
+		}
+		movecols = c("group", "sample_group", "sample_name", "pairs")
 		setcolorder( ans, c( movecols, head(names(ans), -length(movecols)) ) )
 	}
+    if (is.data.table(groups) && !all(c("sample", "group") %in% names(groups)))
+      stop("Columns 'sample' and 'group' should be present in argument 'groups'.")
 	tables = lapply(seq_along(idx1), function(i) extract(idx1[i], idx2[i]))
 	setattr(tables, 'names', hdr)
-
 }
 
-split_sample <- function(sample) {
-	splits = tstrsplit(sample, "_(?=[12]$)", perl=TRUE)
+split_sample <- function(sample_name) {
+	splits = tstrsplit(sample_name, "_(?=[12]$)", perl=TRUE)
 	if (length(splits) == 1L)
 		splits = c(splits, list(1L))
 	if (length(splits) != 2L)
-		stop("'sample' argument should be of the form <samplename>_<pair> for paired end and <samplename> for single end Fastq files, with no '_' elsewhere.")
+		stop("'sample_name' argument should be of the form <samplename>_<pair> for paired end and <samplename> for single end Fastq files, with no '_' elsewhere.")
 	splits[[2]] = as.integer(splits[[2]])
 	splits
 	# x[, c("sample_name", "pairs") := splits]
