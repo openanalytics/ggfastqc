e8a93c9a8ca659d3815bdca45babc2cda5ed268e
Arunkumar Srinivasan <arunkumar.srinivasan@openanalytics.eu>
2016-02-26 01:34:59 +0100

    ggfastqc now requires an annotation file as well. Updated version, added news.

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+v0.1.1
+
+## Features
+
+1. `ggfastqc` now relies on an *annotation* file that conforms to specific format and columns, similar to `ganalyse::rnaseq()`. This enables easier handling of `FastQC` reports coming from different runs.
+
+## Bugs
--- R/fastqc-tables.R
+++ R/fastqc-tables.R
@@ -1,54 +1,81 @@
-#' Extract FastQC summary tables
+#' @title Extract FastQC summary tables
+#' 
+#' @description \code{fastqc} function parses all the various summary statistics produced by \code{FastQC} and returns a nested list data structure corresponding to each sample.
+#' 
+#' @param sample_info Full path to file containing details about \code{samples} and their \code{paths}. See \code{Details} section.
+#' 
+#' @details The file provided to \code{sample_info} argument should contain at least these three columns: 
+#' \itemize{
+#'   \item \code{sample} - contains the \code{sample} name.
+#'   \item \code{pair} - in case of paired end reads, \code{1} or \code{2} corresponding to first and second pair, and in case of single end reads, \code{NA}.
+#'   \item \code{path} - full path to the fastqc summary report (\code{.txt} file) for each sample.
+#' }
+#' 
+#' It can also optionally contain a \code{group} column. If present, then the plot generated with fastqc would be colored / facetted according to samples belonging to the same group.
+#' @seealso \code{\link{plot_dup_stats}}, \code{\link{plot_gc_stats}}, \code{\link{plot_sequence_quality}}, \code{\link{plot_total_sequence_stats}}
 #' @export
-fastqc <- function(dir, groups=NULL) {
-	# not exposing `id` argument here for now.
-	files = list.files(dir, pattern="^fastqc_data\\.txt$", full.names=TRUE, recursive=TRUE)
-	samples = gsub("_fastqc$", "", basename(dirname(files)))
-	all_samples = lapply(seq_along(samples), function(i) fastqc_tables(samples[i], files[i], groups=groups))
-	setattr(all_samples, 'class', 'fastqc')
-	setattr(all_samples, 'names', samples)
+#' @examples
+#' \dontrun{
+#'   require(ggfastqc)
+#'   obj = fastqc("~/Documents/oa/omics/CRC_AZA/fastqc-sample-file.tsv")
+#' }
+fastqc <- function(sample_info) {
+	info = fread(sample_info)
+	cols = c("sample", "pair", "path")
+	rest = setdiff(cols, names(info))
+	if (length(rest)) stop("Columns [", paste(rest, collapse=","), "] not found in file provided to argument 'sample_info'.")
+	pair_uniq = sort(unique(info[["pair"]]))
+	if (length(pair_uniq) == 1L && !is.na(pair_uniq)) {
+		if (pair_uniq != 1L) stop("Pair column must be NA for single end reads, and 1 or 2 for paired end reds.")
+		else warning("Pair column must be NA for single end reads.")
+	} else if ( (length(pair_uniq) == 2L && !identical(pair_uniq, 1:2)) || !length(pair_uniq) %in% 1:2 ) {
+		stop("Pair column must be NA for single end reads, and 1 or 2 for paired end reads.")
+	}
+	if (is.null(info[["group"]])) info[, group := ""]
+	samples = if (length(pair_uniq) == 1L) info[["sample"]] else info[, paste(sample, pair, sep="_")]
+	ans = info[, .(tables = .(extract_summary_tables(readLines(path), sample, pair, group))), by=1:nrow(info)][["tables"]]
+	setattr(ans, 'class', 'fastqc')
+	setattr(ans, 'names', samples)
 }
 
-fastqc_tables <- function(sample_name, fastqc_file, groups=NULL) {
-	doc = readLines(fastqc_file)
-	tables = extract_summary_tables(doc, sample_name, groups)
-}
-
-extract_summary_tables <- function(doc, sample_name, groups=NULL) {
-
-	idx = grep("^>>", doc)
-
-	idx1 = idx[c(TRUE, FALSE)]
-	idx2 = idx[c(FALSE, TRUE)]
-
-	hdr = gsub("^>>(.*)\t.*$", "\\1", doc[idx1])
+#' @title Extract all summary tables for a particular sample
+#' 
+#' @description This is the workhorse that powers \code{fastqc}. Given a \code{sample} and \code{path}, \code{extract_summary_tables} extracts all summary statistics and returns them as a list of \code{data.table}s.
+#' 
+#' @param doc The file corresponding to \emph{that \code{sample}} read in as such (using \code{readLines}).
+#' @param sample Sample name
+#' @param pair 	Read pair
+#' @param group 	Group / condition this sample belongs to
+extract_summary_tables <- function(doc, sample, pair, group) {
 
 	extract <- function(i, j) {
 		if (j-i-2L <= 0L) {
 			 message("Table at start index ", i, " seems to contain only one (header) row, therefore returning empty data.table.")
-			 return(data.table:::null.data.table())
+			 return(setDT(list())[]) # data.table:::null.data.table()
 		}
 		hdr_row = unlist(strsplit(doc[i+1L], "\t", fixed=TRUE))
 		bdy_row = strsplit(doc[(i+2L):(j-1L)], "\t", fixed=TRUE)
 		ans = setDT(transpose(bdy_row))
+		if (ncol(ans) != length(hdr_row)) 
+			hdr_row = c(hdr_row, paste("V", seq_len(ncol(ans)-length(hdr_row)), sep=""))
 		setnames(ans, hdr_row)
-		ans[, sample_name := sample_name
-		  ][, c("sample_group", "pairs") := split_sample(sample_name)]
-		if (is.data.table(groups)) {
-			groups = copy(groups)[, sample_group := as.character(sample)]
-			ans[groups, group := factor(i.group), on="sample_group"]
-		} else {
-			ans[, group := ""]
-		}
-		movecols = c("group", "sample_group", "sample_name", "pairs")
+		ans[, c("sample_group", "pair") := .(sample, pair)]
+		if (!is.na(pair)) ans[, sample_name := paste(sample, pair, sep="_")]
+		ans[, group := group]
+		movecols = c("group", "sample_group", "sample_name", "pair")
 		setcolorder( ans, c( movecols, head(names(ans), -length(movecols)) ) )
 	}
-    if (is.data.table(groups) && !all(c("sample", "group") %in% names(groups)))
-      stop("Columns 'sample' and 'group' should be present in argument 'groups'.")
+
+	idx  = grep("^>>", doc)
+	idx1 = idx[c(TRUE, FALSE)]
+	idx2 = idx[c(FALSE, TRUE)]
+	hdr  = gsub("^>>(.*)\t.*$", "\\1", doc[idx1])
+
 	tables = lapply(seq_along(idx1), function(i) extract(idx1[i], idx2[i]))
 	setattr(tables, 'names', hdr)
 }
 
+# deprecated, but kept for now
 split_sample <- function(sample_name) {
 	splits = tstrsplit(sample_name, "_(?=[12]$)", perl=TRUE)
 	if (length(splits) == 1L)
@@ -57,7 +84,6 @@ split_sample <- function(sample_name) {
 		stop("'sample_name' argument should be of the form <samplename>_<pair> for paired end and <samplename> for single end Fastq files, with no '_' elsewhere.")
 	splits[[2]] = as.integer(splits[[2]])
 	splits
-	# x[, c("sample_name", "pairs") := splits]
-	# setcolorder(x, c("sample_name", "pairs", head(names(x), -2L)))
+	# x[, c("sample_name", "pair") := splits]
+	# setcolorder(x, c("sample_name", "pair", head(names(x), -2L)))
 }
-
