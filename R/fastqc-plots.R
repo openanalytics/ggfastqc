f8b20993bbd9c522a7b81f0a5f4219260aca83ca
Arunkumar Srinivasan <arunkumar.srinivasan@openanalytics.eu>
2016-08-04 08:39:35 +0200

    plot_gc_stats: interactive + more geoms.

--- R/fastqc-plots.R
+++ R/fastqc-plots.R
@@ -1,35 +1,96 @@
 #' @title Plot FastQC GC\% stats
 #' @export
-plot_gc_stats <- function(..., type=c("ggplot2", "plotly")) {
+plot_gc_stats <- function(..., interactive=TRUE, 
+                    geom=c("jitter", "point", "bar")) {
 
     ll = list(...)
-    gc = lapply(ll, function(l) rbindlist(lapply(l, `[[`, "Basic Statistics")))
+    gc = lapply(ll, function(l) {
+            stopifnot(inherits(l, "fastqc"))
+            ans = l[param == "basic_statistics", value]
+            data.table::rbindlist(ans)
+        })
     if (is.null(names(ll)))
-        setattr(gc, 'names', names(ll))
-    gc = rbindlist(gc, idcol=TRUE)[`#Measure` == "%GC"]
-    setnames(gc, "Value", "percent")
-    gc[, "percent" := as.numeric(as.character(percent))
+        setattr(gc, 'names', paste0("fastqc_obj", seq_along(ll)))
+    gc = rbindlist(gc, idcol=TRUE)
+    cols = c("sample_name", "group", ".id")
+    as_factor <- function(x) factor(x, levels=unique(x))
+    gc[, (cols) := lapply(.SD, as_factor), .SDcols=cols
       ][, "splits" := findInterval(1:nrow(gc), seq(1, nrow(gc), by = 26L))]
-    type = match.arg(type)
-    if (!requireNamespace(ggplot2))
-        stop("Package 'ggplot2' is not available.")
-    pl = ggplot(gc, aes(x=sample_name, y=percent)) + 
-            geom_bar(stat="identity", aes(fill=group, group=sample_name)) + 
-            ylim(0, 100) + 
-            facet_wrap(splits ~ .id, scales="free_x", ncol=1L) + 
-            theme_bw() + 
-            theme(axis.text.x = element_text(angle = 45L, hjust = 1, size=14), 
-                axis.text.y = element_text(size = 14),  
-                text = element_text(size=18), 
-                panel.grid.major = element_blank()) + 
-            xlab("Sample") + ylab("GC %") + 
-            scale_fill_brewer(name = "Group", palette="Set1")
-    if (type == "plotly") {
-        if (!requireNamespace(plotly))
-            stop("Package 'plotly' is not available.")
-        pl = plotly::ggplotly(pl)
-    }
-  pl
+    geom = match.arg(geom)
+    pl = switch(geom, 
+        jitter = {
+            if (!interactive) {
+                warn = paste("When geom='jitter', only interactive", 
+                        "plots are possible. Setting interactive=TRUE")
+                warning(warn)
+                interactive=TRUE
+            }
+            if (!requireNamespace("plotly"))
+                stop("Package 'plotly' is not available.")
+            p = ggplot(gc, aes(x=group, y=percent_gc, sample=sample_name)) + 
+                geom_point(position = position_jitter(), aes(fill=group), 
+                    shape=21L, size=4L) + 
+                ylim(0, 100) + xlab("Sample") + ylab("GC %") + theme_bw() + 
+                facet_wrap(~ .id, scales="free_y", ncol=1L) + 
+                theme(axis.text.x = element_text(angle = 45L, hjust = 1L), 
+                    legend.text=element_text(size = 12L), 
+                    legend.background=element_blank(),
+                    legend.key=element_blank(),
+                    panel.grid.major.y=element_line(
+                        colour="#333333", size=0.12), 
+                    panel.grid.major.x=element_blank(), 
+                    panel.grid.minor=element_line(
+                        colour="#999999", size=0.06), 
+                    legend.position="none") + 
+                ggtitle("GC content distribution among samples")
+            plotly::ggplotly(p, tooltip = c("sample", "y"))
+        }, 
+        point = {
+            p = ggplot(gc, aes(x=sample_name, y=percent_gc)) + 
+                geom_point(aes(fill=group), shape=21L, size=4L) + 
+                ylim(0, 100) + xlab("Sample") + ylab("GC %") + theme_bw() + 
+                (if (max(gc$splits) == 1L)
+                    facet_wrap(~ .id, scales="free_y", ncol=1L)
+                else  facet_wrap(splits ~ .id, scales="free_x", ncol=1L)) + 
+                theme(axis.text.x = element_text(angle = 45L, hjust = 1L), 
+                    legend.text=element_text(size = 12L), 
+                    legend.background=element_blank(),
+                    legend.key=element_blank(),
+                    panel.grid.major.y=element_line(
+                        colour="#333333", size=0.12), 
+                    panel.grid.major.x=element_blank(), 
+                    panel.grid.minor=element_line(
+                        colour="#999999", size=0.06), 
+                    legend.position="bottom") + 
+                ggtitle("GC content distribution among samples")
+            if (interactive)
+                p = plotly::ggplotly(p, tooltip = c("x", "y"))
+            p
+        }, 
+        bar = {
+            p = ggplot(gc, aes(x=sample_name, y=percent_gc)) + 
+                geom_bar(stat="identity", aes(fill=group)) + 
+                ylim(0, 100) + xlab("Sample") + ylab("GC %") + theme_bw() + 
+                (if (max(gc$splits) == 1L)
+                    facet_wrap(~ .id, scales="free_y", ncol=1L)
+                else  facet_wrap(splits ~ .id, scales="free_x", ncol=1L)) + 
+                theme(axis.text.x = element_text(angle = 45L, hjust = 1L), 
+                    legend.text=element_text(size = 12L), 
+                    legend.background=element_blank(),
+                    legend.key=element_blank(),
+                    panel.grid.major.y=element_line(
+                        colour="#333333", size=0.12), 
+                    panel.grid.major.x=element_blank(), 
+                    panel.grid.minor=element_line(
+                        colour="#999999", size=0.06), 
+                    legend.position="bottom") + 
+                ggtitle("GC content distribution among samples")
+            if (interactive)
+                p = plotly::ggplotly(p, tooltip = c("x", "y"))
+            p
+        }, 
+    )
+    pl
 }
 
 #' @title Plot FastQC total sequences stats
